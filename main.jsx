f6eacc888c20210187c25ecba765c9bae0ee6702
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-04 22:01:08 +0200

    Milestone 3

--- main.jsx
+++ main.jsx
@@ -1,37 +1,50 @@
 const animaliVari = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
-
 const Componente = () => {
 
     const { useState } = React
     const [animals, setAnimals] = useState([])
+    const [showModal, setShowModal] = useState(false)
+    const [addedAnimal, setAddedAnimal] = useState("")
 
+    const Input = () => {
+        return <input type="text" name="animale" id="animalInput" placeholder="Inserisci un animale" />
+    }
 
-    function randomAnimalGenerator() {
-        const randomNumber = Math.floor(Math.random() * animaliVari.length)
-        const animaleCasuale = animaliVari[randomNumber]
-        if (!animals.includes(animaliVari[randomNumber])) {
-            setAnimals([...animals, animaleCasuale])
-        }
+    function Modal({ title, content, show, onConfirm, onClose }) {
+        return show && ReactDOM.createPortal(
+            <div className="modal-container">
+                <div className="modal">
+                    <h2>{title}</h2>
+                    <div className="modalContent">
+                        {content}
+                    </div>
+                    <button onClick={onClose}>Annulla</button>
+                    <button onClick={onConfirm}>Conferma</button>
+                </div>
+            </div>,
+            document.body
+        )
+    }
+
+    function addAnimal() {
+        setAnimals([...animals, document.getElementById("animalInput").value])
+        setShowModal(false)
     }
 
 
     return (
-
-        <>
-            <button onClick={() => { randomAnimalGenerator() }}>Aggiungi animale</button>
+        <main>
+            <button onClick={() => { setShowModal(true) }}>Aggiungi animale</button>
             <details>
                 <summary><strong>Animali</strong></summary>
-                <ul>
-                    {
-                        animals ? animals.map((element, index) => <li key={index}>{element}</li>) : <li>Nessun animale disponibile</li>
-                    }
-                </ul>
+                <ul>{animals ? animals.map((element, index) => <li key={index}>{element}</li>) : <li>Nessun animale disponibile</li>}</ul>
             </details>
-        </>
-
+            <Modal title={"Aggiungere un animale"} content={<Input />} show={showModal} onClose={() => { setShowModal(false) }} onConfirm={() => { addAnimal() }} />
+        </main>
     )
 }
 
 const elementRoot = ReactDOM.createRoot(document.querySelector(".lista-animali"))
 elementRoot.render(<Componente />)
+
 
