288e530a785907579fe03351365a89f03022a0f1
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-04 20:10:53 +0200

    Milestone 2

--- main.jsx
+++ main.jsx
@@ -1,14 +1,30 @@
-const animals = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
+const animaliVari = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
 
 const Componente = () => {
+
+    const { useState } = React
+    const [animals, setAnimals] = useState([])
+
+
+    function randomAnimalGenerator() {
+        const randomNumber = Math.floor(Math.random() * animaliVari.length)
+        const animaleCasuale = animaliVari[randomNumber]
+        if (!animals.includes(animaliVari[randomNumber])) {
+            setAnimals([...animals, animaleCasuale])
+        }
+    }
+
 
     return (
 
         <>
+            <button onClick={() => { randomAnimalGenerator() }}>Aggiungi animale</button>
             <details>
                 <summary><strong>Animali</strong></summary>
                 <ul>
-                    {animals.map(element => <li>{element}</li>)}
+                    {
+                        animals ? animals.map((element, index) => <li key={index}>{element}</li>) : <li>Nessun animale disponibile</li>
+                    }
                 </ul>
             </details>
         </>
