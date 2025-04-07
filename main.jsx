04635d396d673ba8804f5c90c415c31c72d5eb7f
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-07 16:32:53 +0200

    last push

--- main.jsx
+++ main.jsx
@@ -1,50 +1,225 @@
-const animaliVari = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
+// 📌 Milestone 1: Inserire un Componente React
+// - Monta un componente React all’interno dell’elemento con classe .lista-animali
+/* Il componente deve includere:
+ - Un elemento <details> con titolo "Animali", che contiene:
+ - Una lista <ul> statica che viene creata a partire da un array di stringhe (animals) dove ciascuna stringa rappresenta il nome di un animale.*/
+
+// const animali = ["Corvo", "Maiale", "Papera", "Gatto", "Suricato", "Marmotta"]
+// 
+// const Componente = () => {
+// 
+//     return (
+//         <>
+//             <details>
+//                 <summary>Animali</summary>
+//                 <ul>
+//                     {animali.map(animale => <li>{animale}</li>)}
+//                 </ul>
+//             </details>
+//         </>
+//     )
+// }
+// 
+// ReactDOM.createRoot(document.querySelector(".lista-animali")).render(<Componente />)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// 📌 Milestone 2: Aggiungere Animali Casuali
+// - Trasforma l’array animals usando useState (l’array è inizialmente vuoto).
+// - Aggiungi un bottone "Aggiungi Animale" sopra il <details>.
+// - Cliccando il bottone, un animale casuale viene aggiunto alla lista.
+// - Usa un array predefinito per scegliere casualmente:
+
+// const animaliDefault = ["Corvo", "Maiale", "Papera", "Gatto", "Suricato", "Marmotta"]
+// 
+// const Componente = () => {
+// 
+//     const { useState } = React
+//     const [animals, setAnimals] = useState([])
+// 
+//     function randomAnimal() {
+//         const randomIndex = Math.floor(Math.random() * animaliDefault.length)
+//         const randomAnimal = animaliDefault[randomIndex]
+//         setAnimals([...animals, randomAnimal])
+//     }
+// 
+//     return (
+//         <>
+//             <button onClick={randomAnimal}>Aggiungi Animale</button>
+//             <details>
+//                 <summary>Animali</summary>
+//                 <ul>
+//                     {animals.map((animale, index) => <li key={index}>{animale}</li>)}
+//                 </ul>
+//             </details>
+//         </>
+//     )
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// 📌 Milestone 3: Usare una Modale per Aggiungere Animali
+/*
+Espandilo affinché:
+
+-La vecchia prop content può essere usata per passare un componente qualsiasi.
+-Un nuovo div in fondo alla modale contiene il bottone Annulla e un nuovo bottone Conferma.
+-Una nuova prop onConfirm si aspetta una funzione per gestire l’azione di conferma.
+-Sostituisci l’aggiunta casuale dell’animale con una modale interattiva:
+-Cliccando il bottone "Aggiungi Animale," si apre una modale.
+-La modale include un input di testo (passato al prop content) per inserire il nome di un animale.
+-Conferma: Aggiunge l’animale alla lista e chiude la modale.
+-Annulla: Chiude la modale senza modificare la lista.
+
+Obiettivo: L’utente può aggiungere animali specifici utilizzando la modale.
+*/
+
+// const animaliDefault = ["Corvo", "Maiale", "Papera", "Gatto", "Suricato", "Marmotta"]
+// 
+// const Componente = () => {
+// 
+//     const { useState } = React
+//     const [animals, setAnimals] = useState([])
+//     const [value, setValue] = useState(false)
+// 
+//     function getInput() {
+//         const input = document.getElementById("addAnimal")
+//         setAnimals([...animals, input.value])
+//     }
+// 
+// 
+//     return (
+//         <>
+//             <Modal title={"Inserisci un animale"} content={<input type="text" name="addAnimal" id="addAnimal" placeholder="Inserisci un animale" />} show={value} onClose={() => { setValue(false) }} onConfirm={getInput} />
+//             <button onClick={() => { setValue(true) }}>Aggiungi Animale</button>
+//             <details>
+//                 <summary>Animali</summary>
+//                 <ul>
+//                     {animals.map((animale, index) => <li key={index}>{animale}</li>)}
+//                 </ul>
+//             </details>
+//         </>
+//     )
+// }
+// 
+// 
+// function Modal({
+//     title,
+//     content,
+//     show,
+//     onClose,
+//     onConfirm
+// }) {
+//     return show && ReactDOM.createPortal(
+//         <div className="modal-container">
+//             <div className="modal">
+//                 <h2>{title}</h2>
+//                 <div>{content}</div>
+//                 <div>
+//                     <button onClick={onClose}>Annulla</button>
+//                     <button onClick={onConfirm}>Conferma</button>
+//                 </div>
+//             </div>
+//         </div>,
+//         document.body
+//     )
+// }
+// 
+// ReactDOM.createRoot(document.querySelector(".lista-animali")).render(<Componente />)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// 🎯 Bonus: Utilizzare l'API per Creare Card
+
+const animaliDefault = ["Corvo", "Maiale", "Papera", "Gatto", "Suricato", "Marmotta"]
+
 const Componente = () => {
 
     const { useState } = React
     const [animals, setAnimals] = useState([])
-    const [showModal, setShowModal] = useState(false)
-    const [addedAnimal, setAddedAnimal] = useState("")
+    const [value, setValue] = useState(false)
+    const [loader, setLoader] = useState(false)
 
-    const Input = () => {
-        return <input type="text" name="animale" id="animalInput" placeholder="Inserisci un animale" />
+    async function fetchData(url) {
+        setLoader(true)
+        try {
+            const fetchData = await fetch(url)
+            const objParse = await fetchData.json()
+            return objParse
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setLoader(false)
+        }
+
     }
 
-    function Modal({ title, content, show, onConfirm, onClose }) {
-        return show && ReactDOM.createPortal(
-            <div className="modal-container">
-                <div className="modal">
-                    <h2>{title}</h2>
-                    <div className="modalContent">
-                        {content}
-                    </div>
+    function getInput() {
+        const input = document.getElementById("addAnimal")
+        fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/animals?search=${input.value}`)
+            .then(res => {
+                if (res.length > 0) {
+                    setAnimals([...animals, { name: res[0].name, description: res[0].description, image: res[0].image }])
+                    alert("Animale inserito correttamente")
+                } else {
+                    alert("Animale non trovato")
+                }
+
+            })
+
+        setValue(false)
+    }
+
+
+    return loader === false ? (
+        <>
+            <Modal
+                title={"Inserisci un animale"}
+                content={<input type="text" name="addAnimal" id="addAnimal" placeholder="Inserisci un animale" />}
+                show={value}
+                onClose={() => { setValue(false) }}
+                onConfirm={getInput}
+            />
+            <button onClick={() => { setValue(true) }}>Aggiungi Animale</button>
+            <details>
+                <summary>Animali</summary>
+                <ul>
+                    {animals.map((animale) =>
+                        <>
+                            <li>
+                                <h2>{animale.name}</h2>
+                                <p>{animale?.description ?? "Nessuna descrizione"}</p>
+                                <img src={animale.image} alt="Immagine" />
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </details>
+        </>
+    ) : <><h1>Loading...</h1></>
+}
+
+
+function Modal({
+    title,
+    content,
+    show,
+    onClose,
+    onConfirm
+}) {
+    return show && ReactDOM.createPortal(
+        <div className="modal-container">
+            <div className="modal">
+                <h2>{title}</h2>
+                <div>{content}</div>
+                <div>
                     <button onClick={onClose}>Annulla</button>
                     <button onClick={onConfirm}>Conferma</button>
                 </div>
-            </div>,
-            document.body
-        )
-    }
-
-    function addAnimal() {
-        setAnimals([...animals, document.getElementById("animalInput").value])
-        setShowModal(false)
-    }
-
-
-    return (
-        <main>
-            <button onClick={() => { setShowModal(true) }}>Aggiungi animale</button>
-            <details>
-                <summary><strong>Animali</strong></summary>
-                <ul>{animals ? animals.map((element, index) => <li key={index}>{element}</li>) : <li>Nessun animale disponibile</li>}</ul>
-            </details>
-            <Modal title={"Aggiungere un animale"} content={<Input />} show={showModal} onClose={() => { setShowModal(false) }} onConfirm={() => { addAnimal() }} />
-        </main>
+            </div>
+        </div>,
+        document.body
     )
 }
 
-const elementRoot = ReactDOM.createRoot(document.querySelector(".lista-animali"))
-elementRoot.render(<Componente />)
-
-
+ReactDOM.createRoot(document.querySelector(".lista-animali")).render(<Componente />)
